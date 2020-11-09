8e2ff98ab33720960e649107aba0b29ab6b446dc
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-11-09 16:14:38 -0500

    added code for CPU based sum reduction

--- flemingSumReduction.cu
+++ flemingSumReduction.cu
@@ -7,3 +7,12 @@
 //CONSTANTS
 #define MATRIXSIZE 8
 #define BLOCKSIZE 4
+
+
+//Code to preform sum reduction using the cpu.
+//Overwrites x[0] with the sum of X and returns it as an int
+int SumReduction(int* x, int N){
+	for(i = 1; i < N; i++)
+		x[0] += x[i];
+	return x[0];
+}
